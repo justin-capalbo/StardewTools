247dfd6dfc969b2141ca1c409ac7c0f82e40433a
Justin Capalbo <jastium@gmail.com>
2017-05-19 10:49:27 -0400

    Add basic cask detection
    First round of attempts at parsing the Stardew Valley save file and
    detecting the casks in the basement.  At the moment this is pretty
    hacked and uses the presence of <agingRate> inside of the generic
    <Object> in order to decide if the object is a cask.  This then uses
    linq to xml to access data about the casks like item left and days to
    maturity.  Though it seems buggy at the moment.

--- StardewTools/Stardew.cs
+++ StardewTools/Stardew.cs
@@ -9,17 +9,43 @@ namespace StardewTools
 {
     class Stardew
     {
+
         static void Main(string[] args)
         {
             string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\docs\";
             var doc = XDocument.Load(folder + "Jectia.xml");
-
+            XNamespace ns = "http://www.w3.org/2001/XMLSchema-instance";
             Console.WriteLine(doc.Elements());
 
-            foreach (var child in doc.Element("Cask").Elements())
+            var elements = doc.Elements();
+
+            //Get "Object" nodes 
+            var objects = elements.Descendants("Object");
+
+            int caskCount = 0;
+            foreach (var obj in objects)
             {
-                Console.WriteLine(child.Name);
+                //Determine if the "object" is a cask. 
+                //Needs heavy refactoring - this is a very brittle solution
+                bool cask = false;
+                foreach (var desc in obj.Descendants())
+                {
+                    if (desc.Name == "agingRate")
+                    {
+                        cask = true;
+                        caskCount++;
+                    }
+                }
+
+                if (cask)
+                {
+                    var name = obj.Descendants("heldObject").Descendants("DisplayName").First();
+                    var days = obj.Descendants("daysToMature").First();
+                    Console.WriteLine("==Cask==\nItem: {0}\nDays Left: {1}\n", name, days);
+                }
             }
+            Console.WriteLine("Total Casks: {0}", caskCount);
+            Console.ReadKey();
         }
     }
 }
