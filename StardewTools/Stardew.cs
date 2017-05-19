b5293fb35e035d70f435ad1df888020f32ecb56a
Justin Capalbo <capalbo.justin@gmail.com>
2017-05-19 08:03:26 -0400

    Initial Commit

---
+++ StardewTools/Stardew.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace StardewTools
+{
+    class Stardew
+    {
+        static void Main(string[] args)
+        {
+            string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\docs\";
+            var doc = XDocument.Load(folder + "Jectia.xml");
+
+            Console.WriteLine(doc.Elements());
+
+            foreach (var child in doc.Element("Cask").Elements())
+            {
+                Console.WriteLine(child.Name);
+            }
+        }
+    }
+}
